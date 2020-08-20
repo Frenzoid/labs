14b4b3287e10ba64651b4b1aeb6047b3387a8472
Frenzoid <frenzoid@protonmail.com>
2020-08-20 22:22:15 +0200

    Create hello.java

---
+++ JAVA/hello.java
@@ -0,0 +1,7 @@
+// Simple hello code to test if my VSCode works with Java.
+class Hello {
+    public static void main(String[] args) {
+        var message = "Hello there";
+        System.out.println(message);
+    }
+}
