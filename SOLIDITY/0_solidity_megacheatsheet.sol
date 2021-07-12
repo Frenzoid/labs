e55147685a58725eb14fed26e8c01bb81f1da269
Frenzoid <frenzoid@protonmail.com>
2021-07-12 19:38:06 +0200

    Update 0_solidity_megacheatsheet.sol

--- SOLIDITY/0_solidity_megacheatsheet.sol
+++ SOLIDITY/0_solidity_megacheatsheet.sol
@@ -504,4 +504,11 @@ tx.gasprice;                    //  (uint): gas price of the transaction
 tx.origin;                      //  (address): sender of the transaction (full call chain)
 
 
+/*
+state variables are always in storage
+function arguments are always in memory
+local variables of struct, array or mapping type reference storage by default
+local variables of value type (i.e. neither array, nor struct nor mapping) are stored in the stack
+*/
+
 
