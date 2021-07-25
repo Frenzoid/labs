6170db701a5b569a606a5da8985a9a4eeb48b0b3
Frenzoid <frenzoid@protonmail.com>
2021-07-25 03:46:08 +0200

    Update 0_solidity_megacheatsheet.sol

--- SOLIDITY/0_solidity_megacheatsheet.sol
+++ SOLIDITY/0_solidity_megacheatsheet.sol
@@ -553,3 +553,23 @@ function returnTrue() public pure returns(bool response) {
     return true;
 }
 
+
+
+//  GAS CONSUMPTION PER OPERATION CODE
+Operation         Gas           Description
+
+ADD/SUB           3             Arithmetic operation
+MUL/DIV           5             Arithmetic operation
+ADDMOD/MULMOD     8             Arithmetic operation
+AND/OR/XOR        3             Bitwise logic operation
+LT/GT/SLT/SGT/EQ  3             Comparison operation
+POP               2             Stack operation 
+PUSH/DUP/SWAP     3             Stack operation
+MLOAD/MSTORE      3             Memory operation
+JUMP              8             Unconditional jump
+JUMPI             10            Conditional jump
+SLOAD             200           Storage operation
+SSTORE            5,000/20,000  Storage operation
+BALANCE           400           Get balance of an account
+CREATE            32,000        Create a new account using CREATE
+CALL              25,000        Create a new account using CALL
