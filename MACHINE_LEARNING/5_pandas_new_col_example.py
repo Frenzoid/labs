3552c604445d1a9c79ec9f53b274b3890c23991e
Frenzoid <frenzoid@protonmail.com>
2021-08-21 23:45:18 +0200

    Update 5_pandas_new_col_example.py

--- MACHINE_LEARNING/5_pandas_new_col_example.py
+++ MACHINE_LEARNING/5_pandas_new_col_example.py
@@ -1,1 +1,7 @@
 
+import pandas as pd
+df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
+
+# This creates a new column "male" with the right side return value, for each element in the df array.
+df['male'] = df['Sex'] == 'male'
+print(df.head())
